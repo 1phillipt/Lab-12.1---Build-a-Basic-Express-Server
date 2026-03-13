672ed3e53b5c14d69022fdf7c2f9c44eba3d9705
Phillip Thapa <1thapat@gmail.com>
2026-03-13 18:02:57 -0400

    added typescript config with npm install --save-dev typescript @types/node @types/express npx tsc --init

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import path from 'path';
+
+//creating app  instance
+const app = express();
+
+//defining port
+const PORT = 30005;
+
+app.get('/', (req,res)=>{
+    
+})
