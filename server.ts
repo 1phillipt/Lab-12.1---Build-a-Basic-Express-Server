cb3b0969e9ccc37c2a785839bf92230853aac491
Phillip Thapa <1thapat@gmail.com>
2026-03-27 20:39:00 -0400

    Build basic Express server in TypeScript~

--- server.ts
+++ server.ts
@@ -1,30 +1,22 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-
-//converting url to File paths. need to do this because we are using es modules instaed of commonjs
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-//creating app  instance
 const app = express();
+const PORT = 3000;
 
-//defining port
-const PORT = 30005;
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-
-//sending file to res
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'));
-})
-
-app.get('/contact', (req, res)=>{
-    res.sendFile(path.join(__dirname,'public','contact.html'))
-})
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "contact.html"));
+});
 
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
